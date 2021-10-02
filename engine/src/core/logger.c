890fc23136984f00f9538b7b841c05bd490868c4
yaasin-raki2 <yassin23raki@gmail.com>
2021-10-02 16:42:11 +0100

    added a platform layer where it checks automatically in which platform the app is running and assign the platform api to it's specific platform implementation, for now i just implemented a fraction of the windows implementation(at least i can open a blank window now lol), minor changes to the logger library(logs are now colored based on the log level (just for windows (for now ._.))), added a .clang-format file to format my code as i want based on the LLVM default parameters (I really struggled a lot to find out that i can do that ._.)

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,28 +1,27 @@
 #include "logger.h"
+#include "../platform/platform.h"
 #include "asserts.h"
 
 // TODO: temporary
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdarg.h>
 
-b8 initialize_logging()
-{
+b8 initialize_logging() {
     // TODO: create log file
     return TRUE;
 }
 
-void shutdown_logging()
-{
+void shutdown_logging() {
     // TODO: cleanup logging/write queued entries
 }
 
-void log_output(log_level level, const char *message, ...)
-{
+void log_output(log_level level, const char *message, ...) {
     const char *level_strings[6] = {"[FATAL]: ", "[ERROR]: ", "[WARN]: ", "[INFO]: ", "[DEBUG]: ", "[TRACE]: "};
-    //b8 is_error = level < 2;
+    b8 is_error = level < LOG_LEVEL_WARN;
 
-    char out_message[32000];
+    const i32 msg_length = 32000;
+    char out_message[msg_length];
     memset(out_message, 0, sizeof(out_message));
 
     __builtin_va_list arg_ptr;
@@ -30,14 +29,16 @@ void log_output(log_level level, const char *message, ...)
     vsnprintf(out_message, 32000, message, arg_ptr);
     va_end(arg_ptr);
 
-    char out_message2[64000];
+    char out_message2[msg_length];
     sprintf(out_message2, "%s%s\n", level_strings[level], out_message);
 
-    // TODO: platform-specific output
-    printf("%s", out_message2);
+    // Platform-specific output
+    if (is_error)
+        platform_console_write_error(out_message2, level);
+    else
+        platform_console_write(out_message2, level);
 }
 
-void report_assertion_failure(const char *expression, const char *message, const char *file, i32 line)
-{
+void report_assertion_failure(const char *expression, const char *message, const char *file, i32 line) {
     log_output(LOG_LEVEL_FATAL, "Assertion failed: %s, message: %s file: %s, line: %d\n", expression, message, file, line);
 }
--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,20 +1,25 @@
-#include <core/logger.h>
 #include <core/asserts.h>
-
+#include <core/logger.h>
 #include <stdio.h>
 
-int main(void)
-{
+// TODO: Test
+#include <platform/platform.h>
+
+int main(void) {
     KFATAL("A test message: %f", 13.37f);
     KERROR("A test message: %f", 13.37f);
     KWARN("A test message: %f", 13.37f);
     KINFO("A test message: %f", 13.37f);
     KDEBUG("A test message: %f", 13.37f);
     KTRACE("A test message: %f", 13.37f);
-    printf("line: %d\n", __LINE__);
-    printf("file: %s\n", __FILE__);
 
-    KASSERT(1 == 2);
+    platform_state state;
+    if (platform_startup(&state, "Vulkan Engine", 100, 100, 1280, 720)) {
+        while (TRUE) {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
 
     return (0);
 }
